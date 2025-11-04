ffe26b14593ad05e2c19ac642eb2539aabbcad6b
Doğuser Yarar <72985467+Dousery@users.noreply.github.com>
2025-11-05 00:17:44 +0300

    Remove unused import from app.py
    Removed unused import for ConversationBufferMemory.

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from langchain_community.vectorstores import FAISS
 #from langchain.chains import ConversationalRetrievalChain
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 import os
 import time
